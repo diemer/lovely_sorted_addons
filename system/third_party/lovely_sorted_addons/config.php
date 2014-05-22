7fe0953cec8adb76d9c3358a1a9facbd2388e8b0
Dan Diemer <ddiemer@americanbible.org>
2014-05-22 16:14:08 -0400

    update version number to 1.0.4

--- system/third_party/lovely_sorted_addons/config.php
+++ system/third_party/lovely_sorted_addons/config.php
@@ -2,7 +2,7 @@
 
 /**
  * lovely_sorted_addons config file
- * 
+ *
  * @package	lovely_sorted_addons
  * @author		Dan Diemer - Lovely Addons
  * @copyright	Copyright (c) 2013, Dan Diemer
@@ -13,8 +13,7 @@ if( ! defined('LOVELY_SORTED_ADDONS_NAME'))
 	define('LOVELY_SORTED_ADDONS_NAME',	'Lovely Sorted Add-ons');
 	define('LOVELY_SORTED_ADDONS_DESCRIPTION',	'Sort add-ons into an enabled group and a disabled group.');
 	define('LOVELY_SORTED_ADDONS_PACKAGE',	'lovely_sorted_addons');
-	define('LOVELY_SORTED_ADDONS_VERSION',	'1.0.3');
+	define('LOVELY_SORTED_ADDONS_VERSION',	'1.0.4');
 	define('LOVELY_SORTED_ADDONS_DOCS',	'http://example.com/docs');
-	
+
 }
- 
